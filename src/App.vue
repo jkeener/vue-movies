59929b623db07e4543684fa195483d4b0ceec4a6
James Keener <jimmybobkeenmachine@gmail.com>
2016-12-30 10:39:05 -0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <movies></movies>
+  </div>
+</template>
+
+<script>
+
+import movies from './Movies.vue'
+
+export default {
+    components: {
+    movies
+  },
+  name: 'app',
+  data () {
+    return {
+      title: "James & Alaina's Movies"
+    }
+  }
+}
+</script>
+
+<style>
+    body {
+        font-family: Roboto;
+    }
+    #app {
+        display: flex;
+    }
+</style>
---
+++ src/Movies.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+       <div v-for="image in images" class="images">
+           <img v-bind:src="image.img">
+           <div class="title">{{image.title}}</div>
+       </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        images: [
+            //Add your images and captions here.
+            {img: 'http://lorempixel.com/200/300',title: "Movie 1"},
+            {img: 'http://lorempixel.com/201/300',title: "Movie 2"},
+            {img: 'http://lorempixel.com/200/301',title: "Movie 3"},
+            {img: 'http://lorempixel.com/201/301',title: "Movie 4"},
+            {img: 'http://lorempixel.com/202/300',title: "Movie 5"},
+            {img: 'http://lorempixel.com/200/302',title: "Movie 6"},
+            {img: 'http://lorempixel.com/202/302',title: "Movie 7"},
+            {img: 'http://lorempixel.com/203/300',title: "Movie 8"},
+            {img: 'http://lorempixel.com/200/303',title: "Movie 9"},
+            {img: 'http://lorempixel.com/203/303',title: "Movie 10"},
+        ],
+    };
+  },
+}
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin: 1em;
+    }
+    
+    .images {
+        position: relative;
+        margin: 0 2em 2em 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    
+    .images:hover .title {
+        visibility: visible;
+    }
+    
+    .images:before {
+        position: absolute;
+        content: '';
+        height: 25px;
+        width: 25px;
+        background-color: #fda9b8;
+        z-index: 1;
+        top:-7px; left: -7px;
+    }
+    
+    img {
+        filter: grayscale(100%);
+        transition: .6s;
+    }
+    
+    img:hover {
+        filter: grayscale(0);
+    }
+    
+    .title {
+        visibility: hidden;
+    }
+
+</style>
